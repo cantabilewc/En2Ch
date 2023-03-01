11392ed8e9c6b5ab00f8a933d8e3a3b3d4a38430
wc_chen <cantabilewc@hotmail.com>
2023-03-02 01:11:54 +0800

    Bug fix

--- E2C_ui_4.py
+++ E2C_ui_4.py
@@ -1,8 +1,9 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 import requests
-from bs4 import BeautifulSoup
+import itertools
 
-#在這裡，Ui_MainWindow 這個類別是繼承自 object，因為在定義 Ui_MainWindow 的時候，
+
+# 在這裡，Ui_MainWindow 這個類別是繼承自 object，因為在定義 Ui_MainWindow 的時候，
 # 括號內的 object 表示繼承自 object 這個基底類別。不過，這其實是可以省略的，
 # 因為在 Python 3 中，所有的類別都會自動繼承自 object，所以可以簡化為 class Ui_MainWindow:。
 class Ui_MainWindow(object):
@@ -18,13 +19,13 @@ class Ui_MainWindow(object):
         self.pushButton.clicked.connect(self.startTranslating)
         self.startFlag = [False]  # 將 startFlag 變數宣告為一個 List然後在建立 CopyPlainTextEdit 物件的時候傳遞這個 List。這樣在 mousePressEvent 中就可以透過這個 List 存取到 startFlag 變數，並進行修改。
 
-#setGeometry是所有继承自QWidget的类（例如QPlainTextEdit）都拥有的方法。在这里，
-# CopyPlainTextEdit作为QPlainTextEdit的子类，自然也具有该方法，所以可以在CopyPlainTextEdit对象上调用setGeometry方法。
+        # setGeometry是所有继承自QWidget的类（例如QPlainTextEdit）都拥有的方法。在这里，
+        # CopyPlainTextEdit作为QPlainTextEdit的子类，自然也具有该方法，所以可以在CopyPlainTextEdit对象上调用setGeometry方法。
         self.plainTextEdit_result = QtWidgets.QPlainTextEdit(self.centralwidget) #parent 
         self.plainTextEdit_result.setGeometry(QtCore.QRect(40, 250, 301, 121))
         self.plainTextEdit_result.setObjectName("translating result")
 
-        self.plainTextEdit = CopyPlainTextEdit(self.centralwidget, self.startFlag, self.plainTextEdit_result) #parent object, and argument
+        self.plainTextEdit = CopyPlainTextEdit(self.centralwidget, self.startFlag, self.plainTextEdit_result) # parent object, and arguments
         self.plainTextEdit.setGeometry(QtCore.QRect(40, 120, 301, 121))
 
         MainWindow.setCentralWidget(self.centralwidget)
@@ -53,7 +54,7 @@ class Ui_MainWindow(object):
         else:
             self.pushButton.setText("Start Translating")
 
-#在这个代码中，括号里的参数是用来指定 PlainTextEdit 类的父类或基类的，
+# 在这个代码中，括号里的参数是用来指定 PlainTextEdit 类的父类或基类的，
 # 它告诉 PyQt5 PlainTextEdit 继承了 QtWidgets.QPlainTextEdit 类的所有属性和方法，
 # 使得 PlainTextEdit 可以使用 QtWidgets.QPlainTextEdit 中的所有函数和变量。
 # 通过继承 QtWidgets.QPlainTextEdit 类，可以在这个基础上扩展和自定义控件的功能。
@@ -74,14 +75,14 @@ class Ui_MainWindow(object):
 # 即若在建立物件時未給定該參數，則 parent 會預設為 None，表示該物件沒有父物件，
 # 並可獨立存在。若有需要，則可在建立物件時透過指定 parent 參數的方式指定父物件，使得該物件成為該父物件的子物件。
 class CopyPlainTextEdit(QtWidgets.QPlainTextEdit):
-    def __init__(self, parent=None, startFlag=None, plainTextEdit_result=None):
+    def __init__(self, parent=None, startFlag=None, plainTextEdit_result=None): # 在 Python 中，None 是一個特殊的值，表示沒有值或空值。在這個程式碼中，None 被用來指定函數參數的預設值。在 __init__ 函數中，parent 參數預設值為 None，這意味著如果你不給這個參數傳遞任何值，它就會使用 None 作為預設值。同樣地，startFlag 和 plainTextEdit_result 參數的預設值也是 None，這意味著如果你不給這些參數傳遞任何值，它們也會使用 None 作為預設值。
         super().__init__(parent=parent)
         self.setReadOnly(True)
         self.setObjectName("plainTextEdit")
         self.startFlag = startFlag  # 在建立物件時將 startFlag 變數的 List 存下來
         self.plainTextEdit_result = plainTextEdit_result
 
-#在 PyQt5 中，如果要处理某种类型的事件，通常需要使用对应的事件处理方法，
+# 在 PyQt5 中，如果要处理某种类型的事件，通常需要使用对应的事件处理方法，
 # 并按照 PyQt5 事件处理机制的规范命名，例如 mousePressEvent，keyPressEvent 等等。
 # 这些事件处理方法都有一个固定的参数，即 event，它是 PyQt5 中的事件对象，包含了事件的相关信息。
 # 这个参数名是固定的，不能改变。因此，如果你想处理某种类型的事件，需要按照规范编写相应的事件处理方法，
@@ -100,9 +101,9 @@ class CopyPlainTextEdit(QtWidgets.QPlainTextEdit):
 
     def translate_text(self, text):
         
-        #url = 'https://dict.youdao.com/webtranslate'
+        # url = 'https://dict.youdao.com/webtranslate'
         url = 'http://fanyi.youdao.com/translate?smartresult=dict&smartresult=rule'
-        #這個 URL 是通过在浏览器中使用开发者工具查看网络请求而获得的。在打开有道翻译官网后，使用开发者工具，可以看到请求的 URL，这个 URL 就是请求翻译结果的 URL。这个 URL 是可以使用 HTTP 或 HTTPS 协议的，只需要将请求的方式设置为 POST 就可以。而 URL https://fanyi.youdao.com/index.html# 是有道翻译官网的主页，不是请求翻译结果的 URL。虽然可以在该网页中输入文本并点击翻译按钮，但这个过程涉及到浏览器的 JavaScript 代码，不方便用 Python 直接模拟。
+        # 這個 URL 是通过在浏览器中使用开发者工具查看网络请求而获得的。在打开有道翻译官网后，使用开发者工具，可以看到请求的 URL，这个 URL 就是请求翻译结果的 URL。这个 URL 是可以使用 HTTP 或 HTTPS 协议的，只需要将请求的方式设置为 POST 就可以。而 URL https://fanyi.youdao.com/index.html# 是有道翻译官网的主页，不是请求翻译结果的 URL。虽然可以在该网页中输入文本并点击翻译按钮，但这个过程涉及到浏览器的 JavaScript 代码，不方便用 Python 直接模拟。
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
         }
@@ -124,18 +125,20 @@ class CopyPlainTextEdit(QtWidgets.QPlainTextEdit):
         }
         response = requests.post(url, headers=headers, data=data)
         result = response.json()
-        print(result)
-        total_len = len(result['translateResult'])
-        translation = []
-        for i in range(total_len):
-            translation += [result['translateResult'][i][0]['tgt']]
-        translation = '\n'.join(translation)  # 將列表轉換為字符串，每個元素之間用換行符分隔
-        self.plainTextEdit_result.setPlainText(translation)
+        # print(result)
+        flattened_lst = list(itertools.chain.from_iterable(result['translateResult']))
+        tgt_str = ''
+        for translation in flattened_lst:
+            tgt_str += translation['tgt'] # dictionary搜尋法
+            tgt_str += '\n'
+        # print(tgt_str)  
+        # translation = '\n'.join(translation)  # 將列表轉換為字符串，每個元素之間用換行符分隔
+        self.plainTextEdit_result.setPlainText(tgt_str)
 
 
-if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
+if __name__ == "__main__": # if other scripts import the functions above, this line will not be executed
+    import sys # for the exit function
+    app = QtWidgets.QApplication(sys.argv) # if there are command lines arguments to be sent to the QApplication
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
